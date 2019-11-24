f32401dfb0cc3cc5c69b4bd465ec671af8e64183
Souvik Haldar <souvik@Souviks-MacBook-Pro.local>
2019-11-24 11:17:47 +0530

    Add/Fetch from map complete

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [ring/ring-core "1.6.3"]
-                 [ring/ring-jetty-adapter "1.6.3"]]
+                 [ring/ring-jetty-adapter "1.6.3"]
+                 [compojure "1.6.1"]]
   :main ^:skip-aot web-dev-clojure.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/web_dev_clojure/core.clj
+++ src/web_dev_clojure/core.clj
@@ -1,6 +1,37 @@
 (ns web-dev-clojure.core
-  (:gen-class))
-(defn handler [request]
+  (:gen-class)
+  (:require [compojure.core :refer [defroutes GET POST]]
+            [ring.adapter.jetty :as jetty]))
+(def port ":3000")
+(def host "localhost")
+(def url-map {})
+(def reverse-map {})
+
+(defn rand-str [len]
+  (apply str (take len (repeatedly #(char (+ (rand 40) 65))))))
+
+(defn shorten [request]
+  (def incoming-url (-> request :params :url))
+  (if (contains? reverse-map incoming-url)
+    (get reverse-map incoming-url)
+    (do (def new_url (rand-str 5))
+        (def url-map (assoc url-map new_url incoming-url))
+        (def reverse-map (assoc reverse-map incoming-url new_url))))
   {:status 200
-   :headers {"Content-Type" "text/html"}
-   :body "Hello World!"})
+   :body (str "Shrunken URL is " new_url)
+   :headers {"Content-Type" "text/html"}})
+(defn redirect [request]
+  (def url (-> request :params :url))
+  {:status 200
+   :body (get url-map url)
+   :headers {"Content-Type" "text/html"}})
+
+(defn rand-str [len]
+  (apply str (take len (repeatedly #(char (+ (rand 26) 65))))))
+
+(defroutes redirector
+  (GET "/shorten/:url" [] shorten)
+  (GET "/redirect/:url" [] redirect))
+
+(defn -main []
+  (jetty/run-jetty redirector {:port 3000}))
